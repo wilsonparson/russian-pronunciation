2edd59856eedab9acf1536a215828b78f232173b
Wilson Parson <Wilson Parson>
2017-03-01 04:20:06 -0500

    Template for consistency across pages

---
+++ document-structure.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<?php include 'includes/head.php' ?>	
+	<body>
+        <?php include 'includes/header.php' ?>
+        <?php include 'includes/nav.php' ?>
+        
+        <main>    
+			<h1>Title</h1>
+		
+				<p></p>
+		</main>
+	    <?php include 'includes/footer.php' ?>	
+        <?php include 'includes/'?>	
+	</body>
+</html>
