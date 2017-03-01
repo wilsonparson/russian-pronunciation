a3364860f0995aa1412f4e98110eb143e4648308
Wilson Parson <Wilson Parson>
2017-03-01 04:22:26 -0500

    <audio> and <source> tags to include on particular pages

---
+++ includes/audio-exceptions-always-hard.php
@@ -0,0 +1,30 @@
+<audio id= "zhenaHard">
+    <source  src= "zhenaHard.wav">
+</audio>
+<audio id= "zhenaSoft">
+    <source  src= "zhenaSoft.wav">
+</audio>
+<audio id= "shestHard">
+    <source  src= "shestHard.wav">
+</audio>
+<audio id= "shestSoft">
+    <source  src= "shestSoft.wav">
+</audio>
+<audio id= "tserkovHard">
+    <source  src= "tserkovHard.wav">
+</audio>
+<audio id= "tserkovSoft">
+    <source  src= "tserkovSoft.wav">
+</audio>
+<audio id= "tselHardSoft">
+    <source  src= "tselHardSoft.wav">
+</audio>
+<audio id= "tselSoftHard">
+    <source  src= "tselSoftHard.wav">
+</audio>
+<audio id= "tselSoftSoft">
+    <source  src= "tselSoftSoft.wav">
+</audio>
+<audio id= "tselHardHard">
+    <source  src= "tselHardHard.wav">
+</audio>
---
+++ includes/audio-hard-and-soft-vowels.php
@@ -0,0 +1,30 @@
+<audio>
+    <source src="audio/а.wav">
+</audio>
+<audio>
+    <source src="audio/е.wav">
+</audio>
+<audio>
+    <source src="audio/ё.wav">
+</audio>
+<audio>
+    <source src="audio/и.wav">
+</audio>
+<audio>
+    <source src="audio/о.wav">
+</audio>
+<audio>
+    <source src="audio/у.wav">
+</audio>
+<audio>
+    <source src="audio/ы.wav">
+</audio>
+<audio>
+    <source src="audio/э.wav">
+</audio>
+<audio>
+    <source src="audio/ю.wav">
+</audio>
+<audio>
+    <source src="audio/я.wav">
+</audio>
