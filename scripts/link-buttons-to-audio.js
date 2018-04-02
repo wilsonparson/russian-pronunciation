c43d3aad9aff7f988cd6753ca03107300e61e328
Wilson Parson <wilsonparson@gmail.com>
2018-04-02 15:16:44 -0400

    Update audio file names/data-audio attributes

--- scripts/link-buttons-to-audio.js
+++ scripts/link-buttons-to-audio.js
@@ -4,6 +4,9 @@ for (i=0, ii=buttons.length; i<ii; i++) {
     if (source) { // If there is a corresponding source, add an event listener for the button to play the corresponding audio
         buttons[i].addEventListener("click", function() {
             document.querySelector("source[src='audio/"+this.innerHTML+".wav']").parentElement.play();
-        }); 
+        });
     }
+    // document.querySelectorAll('button').forEach((el) => {
+  //    el.setAttribute('data-audio', `${el.textContent}.wav`);
+  //  });
 }
