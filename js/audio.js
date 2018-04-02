9b87aa681c3f9e238848479ca5decd161123a91f
Wilson Parson <wilsonparson@gmail.com>
2018-04-02 17:05:35 -0400

    Update audio buttons script
    - Use data-audio attribute of buttons to dynamically create audio elements
    - Add click event listeners to buttons

---
+++ js/audio.js
@@ -0,0 +1,17 @@
+appendAudioElements();
+
+function appendAudioElements() {
+  let fragment = document.createDocumentFragment();
+  document.querySelectorAll('[data-audio]').forEach((element) => {
+    let audioSrc = '/audio/' + element.getAttribute('data-audio');
+    element.addEventListener('click', () => {
+      document.querySelector(`audio[src="${audioSrc}"]`).play();
+    });
+
+    let audio = document.createElement('audio');
+    audio.setAttribute('src', audioSrc);
+    audio.setAttribute('preload', 'auto');
+    fragment.appendChild(audio);
+  });
+  document.body.appendChild(fragment);
+}
